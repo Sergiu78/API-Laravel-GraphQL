aa2176449e6761ffcd878dc4146244527489a34a
Sergiu78 <turutaserghei@mail.ru>
2022-05-04 19:09:19 +0300

    mutation

---
+++ app/GraphQL/Mutations/UpdateUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UpdateUser
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $user = User::find($args['id']);
+
+        if(isset($args['name'])) {
+            $user->name = $args['name'];
+        }
+
+        if(isset($args['email'])) {
+            $user->email = $args['email'];
+        }
+        
+        $user->save();
+
+        return $user;
+    }
+}
