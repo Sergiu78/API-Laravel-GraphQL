376a63f79dffca40720ff4c92bc7fc742d5484c3
Sergiu78 <turutaserghei@mail.ru>
2022-05-10 17:46:14 +0300

    uploads file

---
+++ app/GraphQL/Mutations/Upload.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Image;
+use Illuminate\Support\Facades\Storage;
+
+class Upload
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+         /** @var \Illuminate\Http\UploadedFile $file */
+         $file = $args['file'];
+
+         Storage::put('uploads', $file);
+
+         $image = new Image();
+         $image->name = $file->getFilename();
+         $image->hash = $file->hashName();
+
+         $image->save();
+
+         return $image;
+    }
+}
---
+++ app/Models/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected function getPathOfImage($filename, $user_id = null)
+    {
+        $patch = null;
+        if(null != $user_id) {
+            $patch = $user_id . '/';
+        }
+
+        return 'uploads/'. $patch . $filename;
+    }
+
+    public function getUrlAttribute()
+    {
+        
+
+        return Asset($this->getPathOfImage($this->hash, $this->user_id));
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+}
